6998c93298557c1c4c696cb674d9e10181beadb6
Lucas <lucascontee@icloud.com>
2026-08-25 18:21:09 -0300

    feat: add brl currency formater

--- ECommerce.Frontend/src/utils/index.ts
+++ ECommerce.Frontend/src/utils/index.ts
@@ -6,8 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number): string {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('pt-BR', {
         style: 'currency',
-        currency: 'USD',
+        currency: 'BRL',
     }).format(amount);
 }
